e684d34004a230e83ebbc28eb34ff5b2ef183a37
Godina Nicolae <ngodina@ebs.md>
2016-10-30 00:31:22 +0300

    upload layout

---
+++ static/script/main.js
@@ -0,0 +1,23 @@
+$(function () {
+    //BEGIN MENU SIDEBAR
+    $('#sidebar').css('min-height', '100%');
+
+    $(window).bind("load resize", function () {
+        if ($(this).width() < 768) {
+            $('div.sidebar-collapse').addClass('collapse');
+        } else {
+            $('div.sidebar-collapse').removeClass('collapse');
+            $('div.sidebar-collapse').css('height', 'auto');
+        }
+        if($('body').hasClass('sidebar-icons')){
+            $('#menu-toggle').hide();
+        } else{
+            $('#menu-toggle').show();
+        }
+    });
+    //END MENU SIDEBAR
+
+});
+
+
+
